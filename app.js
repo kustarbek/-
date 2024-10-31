a03da3c9671e6016474975387d052b02fe3c1965
kustarbek <kustarbekturdubekov6@gmail.com>
2024-10-31 21:45:06 +0600

    первый коммит

---
+++ app.js
@@ -0,0 +1,45 @@
+
+// 3-js
+
+
+// const  num = prompt('Погода')
+
+// if (20>num) {
+//     console.log(`${num}  Кун суук`);
+//     alert('Кун суук')
+// }else if (35>num) {
+//     console.log(`${num}   Кун жылуу`);
+//     alert('кун жылуу')
+// }else if (50>num){
+//     console.log(`${num} Кун ото ыссык`);
+//     alert('кун ото ыссык')
+// }else{
+//     console.log(`${num} Смертельно высокая температура`);
+//     alert('Смертельно высокая температура')  
+// }
+
+// console.log(`+${num}`);
+
+
+
+// 4-js
+
+
+
+
+
+
+// 6-js
+
+
+// for ( let i = 0; i <= 500; i++){
+//     if (i % 3 === 0) {
+//         console.log(i, `Coca`);
+//     }else if (i % 5 === 0){ 
+//         console.log(i, `Cola`);
+//     }if (i % 15 === 0){
+//         console.log(i, `Coca-cola`);      
+//     }else{
+//         console.log(i);    
+//     }
+// }
